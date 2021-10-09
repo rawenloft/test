43f36e56308b672f8f291de9519281ac56fd642b
rawenloft <>
2021-10-09 23:40:28 +0300

    Make mails view and sent funcs.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,9 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.querySelector('#send-mail').addEventListener('click', send_mail);
+  
   // By default, load the inbox
   load_mailbox('inbox');
+  document.querySelector('#emails-view').classList.add('container');
+
 });
 
 function compose_email() {
@@ -30,4 +33,55 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  // Show emails
+  fetch(`/emails/${mailbox}`).then(response => response.json()).then(emails => {
+    open_mail(emails);
+    emails.forEach(email => {
+      make_email(email);
+    })
+  })
+  
+}
+
+function send_mail(){
+  fetch('/emails', {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject:document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  }).then(response => response.json()).then(result => {
+    console.log(result);
+  });
+  return false;
+}
+
+function make_email(email){
+  const mail_elem = document.createElement('div');
+  const sender = document.createElement('span');
+  const subj = document.createElement('span');
+  const timestamp = document.createElement('span');
+
+  mail_elem.classList.add('row','mail','rounded','read');
+  sender.classList.add('col-lg-2','col-sm-6');
+  subj.classList.add('col-lg-7','col-sm-6');
+  timestamp.classList.add('col-lg-3','col-sm-12');
+
+  sender.innerHTML = email.sender;
+  subj.innerHTML = email.subject;
+  timestamp.innerHTML = email.timestamp;
+
+  mail_elem.append(sender)
+  mail_elem.append(subj)
+  mail_elem.append(timestamp)
+
+  mail_elem.addEventListener('click', () => {
+    console.table(email)
+  });
+  document.querySelector('#emails-view').append(mail_elem);
+}
+
+function open_mail(emails){
+  return
 }
